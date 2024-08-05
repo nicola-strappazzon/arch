7859baa6efd1eb2b0437dbfdb8db3321062632c3
nstrappazzonc <nicola@strappazzon.me>
2024-08-05 23:51:28 +0200

    Save changes at: 1722894688

--- get.sh
+++ get.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 set -eu
 
+declare -A osInfo;
+declare platform;
+declare arch;
+
 main() {
     check_is_not_root
     platform="$(uname -s)"
     arch="$(uname -m)"
+    check_valid_platform
+    check_valid_platform_architecture
+    check_exist_curl_or_wget
+    tmp="$(mktemp -d "/tmp/strappazzon-XXXXXX")"
     pms=($(package_manager_system))
 
     echo "OS: ${platform}-${arch}"
@@ -12,16 +20,54 @@ main() {
 }
 
 check_is_not_root() {
-    # Make sure are not root can run our script
     if [[ $EUID -eq 0 ]]; then
         echo "This script must be run as not root"
         exit 1
     fi
 }
 
-package_manager_system() {
-    declare -A osInfo;
+check_valid_platform() {
+    if [ "$platform" = "Darwin" ]; then
+        platform="macos"
+    elif [ "$platform" = "Linux" ]; then
+        platform="linux"
+    else
+        echo "Unsupported platform $platform"
+        exit 1
+    fi
+}
 
+check_valid_platform_architecture() {
+    case "$platform-$arch" in
+        macos-arm64* | linux-arm64* | linux-armhf | linux-aarch64)
+            arch="aarch64"
+            ;;
+        macos-x86* | linux-x86* | linux-i686*)
+            arch="x86_64"
+            ;;
+        *)
+            echo "Unsupported platform or architecture"
+            exit 1
+            ;;
+    esac
+}
+
+check_exist_curl_or_wget(){
+    if which curl >/dev/null 2>&1; then
+        curl () {
+            command curl -sfL "$@"
+        }
+    elif which wget >/dev/null 2>&1; then
+        curl () {
+        wget -O- "$@"
+        }
+    else
+        echo "Could not find 'curl' or 'wget' in your path"
+        exit 1
+    fi
+}
+
+package_manager_system() {
     osInfo[/etc/redhat-release]=yum
     osInfo[/etc/arch-release]=pacman
     osInfo[/etc/gentoo-release]=emerge
