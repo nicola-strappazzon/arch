2ae7438b693b2cd4b9449d20217fbd318693f54a
nstrappazzonc <nicola@strappazzon.me>
2024-08-05 23:44:10 +0200

    Save changes at: 1722894250

--- get.sh
+++ get.sh
@@ -2,12 +2,21 @@
 set -eu
 
 main() {
+    check_is_not_root
     platform="$(uname -s)"
     arch="$(uname -m)"
     pms=($(package_manager_system))
 
     echo "OS: ${platform}-${arch}"
     echo "Package manager: ${pms}"
+}
+
+check_is_not_root() {
+    # Make sure are not root can run our script
+    if [[ $EUID -eq 0 ]]; then
+        echo "This script must be run as not root"
+        exit 1
+    fi
 }
 
 package_manager_system() {
