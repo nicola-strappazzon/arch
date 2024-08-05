2befb1497d7b432714cc24d2a183077d7d913bd5
nstrappazzonc <nicola@strappazzon.me>
2024-08-05 23:53:46 +0200

    Save changes at: 1722894826

--- get.sh
+++ get.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-declare -A osInfo;
+declare -A osPMS;
 declare platform;
 declare arch;
 
@@ -68,17 +68,17 @@ check_exist_curl_or_wget(){
 }
 
 package_manager_system() {
-    osInfo[/etc/redhat-release]=yum
-    osInfo[/etc/arch-release]=pacman
-    osInfo[/etc/gentoo-release]=emerge
-    osInfo[/etc/SuSE-release]=zypp
-    osInfo[/etc/debian_version]=apt-get
-    osInfo[/etc/alpine-release]=apk
+    osPMS[/etc/redhat-release]=yum
+    osPMS[/etc/arch-release]=pacman
+    osPMS[/etc/gentoo-release]=emerge
+    osPMS[/etc/SuSE-release]=zypp
+    osPMS[/etc/debian_version]=apt-get
+    osPMS[/etc/alpine-release]=apk
 
-    for f in ${!osInfo[@]}
+    for f in ${!osPMS[@]}
     do
         if [[ -f $f ]];then
-            echo "${osInfo[$f]}"
+            echo "${osPMS[$f]}"
         fi
     done
 }
