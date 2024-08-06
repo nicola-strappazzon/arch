04d5ced136ae348edaf1860ebb1d12247a3cc8c9
Nicola Strappazzon. <nicola@strappazzon.me>
2024-08-07 00:29:50 +0200

    Save changes at: 1722983390

--- profile/bashrc.sh
+++ profile/bashrc.sh
@@ -8,7 +8,11 @@ mkdir -p ~/.bashrc.d/functions/
 cp -R "$(pwd)/profile/bashrc/." ~/.bashrc.d/
 chmod -R 0700 ~/.bashrc.d
 
+if [ ! -f /etc/profile.d/custom.sh ]; then
+    sudo cp $(pwd)/profile/bashrc/custom.sh /etc/profile.d/custom.sh
+fi
+
 if [ -f ~/.bashrc ]; then
-    grep -qxF '[[ -f /etc/profile.d/bashrc.sh ]] && . /etc/profile.d/bashrc.sh' ~/.bashrc || \
-    echo '[[ -f /etc/profile.d/bashrc.sh ]] && . /etc/profile.d/bashrc.sh' >> ~/.bashrc
+    grep -qxF '[[ -f /etc/profile.d/custom.sh ]] && . /etc/profile.d/custom.sh' ~/.bashrc || \
+    echo '[[ -f /etc/profile.d/custom.sh ]] && . /etc/profile.d/custom.sh' >> ~/.bashrc
 fi
--- profile/uninstall.sh
+++ profile/uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-sudo rm -rf /etc/profile.d/bashrc.sh
+sudo rm -rf /etc/profile.d/custom.sh
 rm -rf ~/.bashrc.d/
 rm -rf ~/.gitconfig
-sed -i '/[[ -f \/etc\/profile.d\/bashrc.sh ]] && . \/etc\/profile.d\/bashrc.sh/d' ~/.bashrc
+sed -i '/[[ -f \/etc\/profile.d\/custom.sh ]] && . \/etc\/profile.d\/custom.sh/d' ~/.bashrc
