e2b22c5fd612ae3a7719a832e99f37ce710f5fe5
Nicola Strappazzon. <nicola@strappazzon.me>
2024-08-06 21:15:05 +0200

    Save changes at: 1722971705

--- os/pacman/docker.sh
+++ os/pacman/docker.sh
@@ -3,12 +3,12 @@ set -eu
 
 declare -I EXITCODE=0
 
-pacman -Q "${PACKAGE}" &> /dev/null || EXITCODE=$?
+pacman -Q docker &> /dev/null || EXITCODE=$?
 
 if [ "${EXITCODE}" -ne 0 ]; then
     sudo pacman -S docker --noconfirm --needed
     sudo systemctl start docker.service
     sudo systemctl enable docker.service
-    newgrp docker
+    sudo newgrp docker
     sudo usermod -aG docker $USER
 fi
