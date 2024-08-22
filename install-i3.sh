b3c7a9121a1a443d29f2d4e3e79d79423a8d1e97
Nicola Strappazzon <ns@workstation.localdomain>
2024-08-22 21:50:23 +0200

    Save changes at: 1724356223

--- install-i3.sh
+++ install-i3.sh
@@ -115,17 +115,18 @@ launcher() {
 packages() {
     echo "--> Install desktop packages."
     sudo pacman -S --noconfirm --needed \
-        cheese      `#Webcam GUI`     \
-        evince      `#PDF viewer`     \
-        firefox     `#WEB browser`    \
-        flameshot   `#Screenshot`     \
-        mpv         `#Video player`   \
-        nemo        `#File manager`   \
-        rhythmbox   `#Audio player`   \
-        viewnior    `#Image viewer`   \
-        arduino-ide `#Arduino IDE`    \
-        nicotine+   `#Music sharing`  \
-        texmaker    `#LaTex editor`   \
+        cheese      `#Webcam GUI`               \
+        evince      `#PDF viewer`               \
+        firefox     `#WEB browser`              \
+        flameshot   `#Screenshot`               \
+        mpv         `#Video player`             \
+        nemo        `#File manager`             \
+        rhythmbox   `#Audio player`             \
+        viewnior    `#Image viewer`             \
+        arduino-ide `#Arduino IDE`              \
+        nicotine+   `#Music sharing client`     \
+        texmaker    `#LaTex editor`             \
+        qemu-full   `#Virtual Machine emulator` \
     &> /dev/null
 }
 
