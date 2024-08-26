02eed4e0626c0ef1c3e344677a9a1516384323cd
Nicola Strappazzon. <nicola@strappazzon.me>
2024-08-26 13:22:23 +0200

    Save changes at: 1724671343

--- desktop.sh
+++ desktop.sh
@@ -29,6 +29,7 @@ main() {
     configure_brightness
     configure_feh
     configure_applications_desktop
+    configure_mocp
 }
 
 update() {
@@ -1146,6 +1147,8 @@ configure_feh() {
 }
 
 configure_applications_desktop() {
+    echo "--> Configure applications desktop."
+
     sudo rm -f /usr/share/applications/*
 
     cat << EOF | sudo tee /usr/share/applications/sublime_text.desktop &> /dev/null
@@ -1340,6 +1343,50 @@ MimeType=image/bmp;image/gif;image/jpeg;image/jpg;image/pjpeg;image/png;image/ti
 Keywords=Image;Picture;Slideshow;
 EOF
 
+}
+
+configure_mocp() {
+    echo "--> Configure moc themme."
+
+    mkdir -p /home/nsc/.moc/themes
+
+    cat > /home/nsc/.moc/themes/main << 'EOF'
+background           = default default
+frame                = default default
+window_title         = default default
+directory            = cyan    default
+selected_directory   = cyan    default reverse
+playlist             = default default
+selected_playlist    = default default reverse
+file                 = default default
+selected_file        = default default reverse
+marked_file          = cyan    default bold
+marked_selected_file = cyan    default reverse
+info                 = default default
+selected_info        = default default
+marked_info          = cyan    default bold
+marked_selected_info = cyan    default bold
+status               = default default
+title                = cyan    default bold
+state                = default default
+current_time         = default default
+time_left            = default default
+total_time           = default default
+time_total_frames    = default default
+sound_parameters     = default default
+legend               = default default
+disabled             = default default
+enabled              = cyan    default bold
+empty_mixer_bar      = default default
+filled_mixer_bar     = default default reverse
+empty_time_bar       = default default
+filled_time_bar      = default default reverse
+entry                = default default
+entry_title          = default default
+error                = default default bold
+message              = default default bold
+plist_time           = default default
+EOF
 }
 
 main "$@"
