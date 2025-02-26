7568ed02e2c1d9d0c1fd34a2c891b78320c2e5c2
Nicola Strappazzon. <nicola@strappazzon.me>
2025-02-26 20:44:15 +0100

    Save changes at: 1740599055

--- vol.sh
+++ vol.sh
@@ -11,18 +11,20 @@ function partitioning() {
     readarray -t VOLUMES < <(lsblk --list --nvme --nodeps --ascii --noheadings --output=NAME | sort)
 
     echo "--> Available volumes:"
-    echo "  > Choice volume to install: "
-    IFS="" read -r -s -p "    Enter your password: " VOLUMEN </dev/tty
-
-    select VOLUMEN in "${VOLUMES[@]}"; do
-        if [[ -z $VOLUMEN ]]; then
-            echo "    Invalid choice, try again."
-        else
-            echo "  > Has chosen this volume: $VOLUMEN"
-            VOLUMEN="/dev/${VOLUMEN}"
-            break
-        fi
+    for VOLUMEN in "${VOLUMES[@]}"; do
+        echo "  - ${VOLUMEN}"
     done
+    IFS="" read -r -s -p "  > Choice volume to install: " VOLUMEN </dev/tty
+
+#     select VOLUMEN in "${VOLUMES[@]}"; do
+#         if [[ -z $VOLUMEN ]]; then
+#             echo "    Invalid choice, try again."
+#         else
+#             echo "  > Has chosen this volume: $VOLUMEN"
+#             VOLUMEN="/dev/${VOLUMEN}"
+#             break
+#         fi
+#     done
 
     echo "--> Umount partitions."
 }
