3a475bb2b727dcd77116d0cb544c9b73045ad226
Nicola Strappazzon. <nicola@strappazzon.me>
2025-02-26 22:48:14 +0100

    Save changes at: 1740606494

--- vol.sh
+++ vol.sh
@@ -23,7 +23,44 @@ function partitioning() {
 
     VOLUMEN="/dev/${VOLUMES_LIST[$VOLUMEN_ID]}"
     echo "  > Has chosen this volume: $VOLUMEN"
+
     echo "--> Umount partitions."
+    (umount --all-targets --quiet --recursive /mnt/) || true
+    (swapoff --all) || true
+
+    echo "--> Delete old partitions."
+    (parted --script $VOLUMEN rm 1 &> /dev/null) || true
+    (parted --script $VOLUMEN rm 2 &> /dev/null) || true
+    (parted --script $VOLUMEN rm 3 &> /dev/null) || true
+
+    echo "--> Create new partitions."
+    parted --script $VOLUMEN mklabel gpt
+    parted --script $VOLUMEN mkpart efi fat32 1MiB 1024MiB
+    parted --script $VOLUMEN set 1 esp on
+    parted --script $VOLUMEN mkpart swap linux-swap 1GiB 32GiB
+    parted --script $VOLUMEN mkpart root ext4 32GiB 100%
+
+    echo "--> Format partitions."
+    mkfs.fat -F32 -n UEFI "${VOLUMEN}p1" &> /dev/null
+    mkswap -L SWAP "${VOLUMEN}p2" &> /dev/null
+    mkfs.ext4 -L ROOT "${VOLUMEN}p3" &> /dev/null
+
+    echo "--> Verify partitions."
+    partprobe $VOLUMEN
+
+    echo "--> Mount: swap, root and boot"
+    swapon "${VOLUMEN}p2"
+    mount "${VOLUMEN}p3" /mnt
+    mkdir -p /mnt/boot/efi/
+    mount "${VOLUMEN}p1" /mnt/boot/efi/
+
+    echo "--> Remove default directories lost+found."
+    rm -rf /mnt/boot/efi/lost+found
+    rm -rf /mnt/lost+found
+
+    echo "--> Generate fstab."
+    mkdir /mnt/etc/
+    genfstab -pU /mnt >> /mnt/etc/fstab
 }
 
 main "$@"
