1b1191064ae9895081d0cdcb6468615838fc3d3a
Nicola Strappazzon. <nicola@strappazzon.me>
2025-02-26 20:54:41 +0100

    Save changes at: 1740599681

--- vol.sh
+++ vol.sh
@@ -8,18 +8,11 @@ function main() {
 }
 
 function partitioning() {
-# Scriptname: runit
-    PS3="Select a program to execute: "
-    select program in 'ls -F' pwd date cal exit
-    do
-    $program
-    done
-
     readarray -t VOLUMES < <(lsblk --list --nvme --nodeps --ascii --noheadings --output=NAME | sort)
 
     echo "--> Available volumes:"
-    for VOLUMEN in "${VOLUMES[@]}"; do
-        echo "    - ${VOLUMEN}"
+    for VOLUMEN in "${!VOLUMES[@]}"; do
+        echo "  1. ${VOLUMEN}"
     done
     IFS="" read -r -p "  > Choice volume to install: " VOLUMEN </dev/tty
     if [[ -z $VOLUMEN ]]; then
