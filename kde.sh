ba6f5b948c4f303c56f0d2e716624353a61b5350
Nicola Strappazzon. <nicola@strappazzon.me>
2025-03-11 00:16:34 +0100

    Save changes at: 1741648594

--- kde.sh
+++ kde.sh
@@ -26,43 +26,41 @@ function desktop() {
 function packages() {
     echo "--> Install packages."
     sudo pacman -S --noconfirm --needed \
-        ark                   `#Archiving Tool`             \
-        digikam               `#Digital photo management`   \
-        dolphin               `#File manager`               \
-        elisa                 `#Music player`               \
-        firefox               `#Web browser`                \
-        gwenview              `#Image viewer`               \
-        hunspell-en_us        `#Spell dictionary EN`        \
-        hunspell-es_es        `#Spell dictionary ES`        \
-        kamoso                `#Camera tool`                \
-        kate                  `#Code editor`                \
-        kbackup               `#Backup tool`                \
-        kcalc                 `#Calculator`                 \
-        kcharselect           `#select special characters`  \
-        kcolorchooser         `#color palette tool`         \
-        kfind                 `#Find files`                 \
-        kid3                  `#Audio tag editor`           \
-        kmix                  `#Sound channel mixer`        \
-        konsole               `#Terminal client`            \
-        ksystemlog            `#Show all logs`              \
-        ktorrent              `#Torrent client`             \
-        okteta                `#Hex editor`                 \
-        okular                `#Document viewer`            \
-        partitionmanager      `#Partition Manager`          \
-        powerdevil            `#Power consumption settings` \
-        rssguard              `#RSS Client`                 \
-        spectacle             `#Screenshots`                \
-        system-config-printer `#Print settings`             \
-        thunderbird           `#Email client`               \
+        ark                   `# Archiving Tool`             \
+        digikam               `# Digital photo management`   \
+        dolphin               `# File manager`               \
+        elisa                 `# Music player`               \
+        firefox               `# Web browser`                \
+        gwenview              `# Image viewer`               \
+        hunspell-en_us        `# Spell dictionary EN`        \
+        hunspell-es_es        `# Spell dictionary ES`        \
+        kamoso                `# Camera tool`                \
+        kate                  `# Code editor`                \
+        kbackup               `# Backup tool`                \
+        kcalc                 `# Calculator`                 \
+        kcharselect           `# Select special characters`  \
+        kcolorchooser         `# color palette tool`         \
+        kfind                 `# Find files`                 \
+        kid3                  `# Audio tag editor`           \
+        kmix                  `# Sound channel mixer`        \
+        konsole               `# Terminal client`            \
+        ksystemlog            `# Show all logs`              \
+        ktorrent              `# Torrent client`             \
+        okteta                `# Hex editor`                 \
+        okular                `# Document viewer`            \
+        partitionmanager      `# Partition Manager`          \
+        powerdevil            `# Power consumption settings` \
+        rssguard              `# RSS Client`                 \
+        spectacle             `# Screenshots`                \
+        system-config-printer `# Print settings`             \
+        thunderbird           `# Email client`               \
     &> /dev/null
 }
 
 function configure() {
     kwriteconfig6 --file "$HOME"/.config/kdeglobals --group "Sounds" --key "Enable" "false"
-
     kwriteconfig6 --file "$HOME"/.config/ksplashrc --group "KSplash" --key "Engine" "None"
     kwriteconfig6 --file "$HOME"/.config/ksplashrc --group "KSplash" --key "Theme" "None"
-
     kwriteconfig6 --file "$HOME"/.config/kwalletrc --group "Wallet" --key "Enabled" "false"
 
     kscreen-doctor output.HDMI-A-1.scale.1
